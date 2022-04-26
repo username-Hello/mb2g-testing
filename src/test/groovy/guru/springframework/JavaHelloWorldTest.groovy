451a77186c8c435956a4f741d9974d86da0411b5
username-Hello <nikitos.burak@gmail.com>
2022-04-26 22:07:28 +0300

    adding spock

---
+++ src/test/groovy/guru/springframework/JavaHelloWorldTest.groovy
@@ -0,0 +1,14 @@
+package guru.springframework
+
+import spock.lang.Specification
+
+class JavaHelloWorldSpecTest extends Specification {
+
+    def "GetHello"() {
+        when:
+        def foo = "123"
+
+        then:
+        true
+    }
+}
